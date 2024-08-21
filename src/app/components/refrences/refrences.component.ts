bff749f35bdccaeb7606292906fb5b2734abf92d
NaadaMahmoud <119589202+NaadaMahmoud@users.noreply.github.com>
2024-08-21 12:40:40 +0300

    refactor code

--- src/app/components/refrences/refrences.component.ts
+++ src/app/components/refrences/refrences.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./refrences.component.css']
 })
 export class RefrencesComponent {
-
+  references = [
+    { imageSrc: './assets/images/home/digital-signature.png', altText: 'Digital Signature', title: 'Digital signature', description: 'transformation' },
+    { imageSrc: './assets/images/home/banking-system.png', altText: 'Billing System', title: 'Billing System', description: '_transformation' },
+    { imageSrc: './assets/images/home/id-card.png', altText: 'Students Smart Cards', title: 'Students smart', description: 'cards for HOME' },
+    { imageSrc: './assets/images/home/agreement.png', altText: 'Ministry of Finance', title: 'Ministry of', description: 'finance' }
+  ];
 }
